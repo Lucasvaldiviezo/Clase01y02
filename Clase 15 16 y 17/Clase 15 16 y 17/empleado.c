1eeb643617fb4e636616af0eb87ea05d15c846f7
sofia <sofia@sofia.com>
2018-10-17 19:57:34 -0300

    Empezando con Punteros

---
+++ Clase 15 16 y 17/Clase 15 16 y 17/empleado.c
@@ -0,0 +1,121 @@
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include "empleado.h"
+#include <ctype.h>
+static int isValidNombre(char* nombre);
+static int isValidApellido(char* apellido);
+static int isValidAltura(float altura);
+
+Empleado* empleado_new(void)
+{
+
+    return (Empleado*) malloc(sizeof(Empleado));
+}
+
+void empleado_delete(Empleado* this)
+{
+    free(this);
+}
+
+int empleado_setNombre(Empleado* this,char* nombre)
+{
+    int retorno=-1;
+
+    if(this != NULL && isValidNombre(nombre))
+    {
+        strcpy(this->nombre,nombre);
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+int empleado_getNombre(Empleado* this,char* nombre)
+{
+    int retorno=-1;
+
+    if(this != NULL && nombre != NULL)
+    {
+        strcpy(nombre,this->nombre);
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+static int isValidNombre(char* nombre)
+{
+    int retorno=1;
+    return retorno;
+}
+
+int empleado_compareEmpleado(Empleado* thisA, Empleado* thisB)
+{
+    return strcmp(thisA->nombre,thisB->nombre);
+}
+
+int empleado_setApellido(Empleado* this,char* apellido)
+{
+    int retorno=-1;
+
+    if(this != NULL && isValidApellido(apellido))
+    {
+        strcpy(this->apellido,apellido);
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+int empleado_getApellido(Empleado* this,char* apellido)
+{
+    int retorno=-1;
+
+    if(this != NULL && apellido != NULL)
+    {
+        strcpy(apellido,this->apellido);
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+static int isValidApellido(char* apellido)
+{
+    int retorno=1;
+    return retorno;
+}
+
+int empleado_setAltura(Empleado* this,float altura)
+{
+    int retorno=-1;
+
+    if(this != NULL && isValidAltura(altura))
+    {
+        this->altura=altura;
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+int empleado_getAltura(Empleado* this,float* altura)
+{
+    int retorno=-1;
+
+    if(this != NULL && altura != NULL)
+    {
+        *altura=this->altura;
+        retorno=0;
+    }
+
+    return retorno;
+}
+
+static int isValidAltura(float altura)
+{
+    int retorno=1;
+    return retorno;
+}
+
---
+++ Clase 15 16 y 17/Clase 15 16 y 17/empleado.h
@@ -0,0 +1,34 @@
+#ifndef EMPLEADO_H_INCLUDED
+#define EMPLEADO_H_INCLUDED
+
+typedef struct
+{
+    char nombre[50];
+    char apellido[50];
+    float altura;
+
+    int idEmpleado;
+
+}Empleado;
+
+Empleado* empleado_new();
+Empleado* empleado_newConParametro(char* nombre, char* apellido,float altura);
+void empleado_delete(Empleado* this);//This se refiere al que le acabo de pasar
+
+int empleado_setNombre(Empleado* this,char* nombre);
+int empleado_getNombre(Empleado* this,char* nombre);//en este caso, el char nombre es donde le devolvemos el nombre
+
+int empleado_setApellido(Empleado* this,char* apellido);
+int empleado_getApellido(Empleado* this,char* apellido);//en este caso, el char apellido es donde le devolvemos el apellido
+
+int empleado_setAltura(Empleado* this,float altura);
+int empleado_getAltura(Empleado* this,float* altura);//en este caso, el float de altura recibe un puntero para guardar esa altura a devolver
+
+int empleado_compareEmpleado(Empleado* thisA, Empleado* thisB);
+//vamos a tener el constructor(new)
+//el destructor(delete)
+//setNombre o Apellido: recibe un nombre y lo guarda en un empleado y valida si el nombre es correcto
+//getNombre o Apellido: Obtiene el nombre y lo devuelve
+//Vamos a validar con IsValid(isLetra, isInt, isFloat)
+
+#endif // EMPLEADO_H_INCLUDED
