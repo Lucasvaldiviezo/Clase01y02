fcab64b624e01dbbb9ff730ccda0f90bc26dd918
sofia <sofia@sofia.com>
2018-10-24 20:03:24 -0300

    avances

--- Clase 18/Clase 18/main.c
+++ Clase 18/Clase 18/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char bufferId[1024];
+    char bufferNombre[1024];
+    char bufferApellido[1024];
     FILE *pArchivo;
+/*    pArchivo=fopen("archivo.ext","w");
+    if(pArchivo!=NULL)
+    {
+        fprintf(pArchivo,"Id: %d,Nombre: %s,Apellido: %s\n",id,nombre,apellido);
+        fprintf(pArchivo,"Id: %d,Nombre: %s,Apellido: %s\n",id+1,nombre,apellido);
+        fprintf(pArchivo,"Id: %d,Nombre: %s,Apellido: %s\n",id+2,nombre,apellido);
+        fclose(pArchivo);
+    }else
+    {
+        printf("El archivo no existe");
+    }
+*/
     pArchivo=fopen("archivo.ext","r");
     if(pArchivo!=NULL)
     {
-        printf("Abrio");
+        while(!feof(pArchivo))
+        {
+            strcpy(bufferApellido,"----------------");
+            fscanf(pArchivo,"%[^,],%[^,],%[^\n]\n",bufferId,bufferNombre,bufferApellido);
+            printf("%s - %s - %s\n",bufferId,bufferNombre,bufferApellido);
+        }
+
         fclose(pArchivo);
     }else
     {
