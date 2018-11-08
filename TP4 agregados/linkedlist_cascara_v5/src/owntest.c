d88534330ad6c653d34f415eac6cd251da480535
sofia <sofia@sofia.com>
2018-11-08 20:39:56 -0300

    Filter del TP4

---
+++ TP4 agregados/linkedlist_cascara_v5/src/owntest.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../testing/inc/main_test.h"
+#include "../inc/LinkedList.h"
+#include "../testing/inc/Employee.h"
+#define CANT_EMPLEADOS 10
+#include "owntest.h"
+
+void testFilter(void)
+{
+    LinkedList* test;
+        test=ll_newLinkedList();
+        LinkedList* newTest;
+        Employee* auxEmployee;
+        int i;
+        int size;
+        if(test != NULL)
+        {
+            printf("La lista sin filtrar es: \n");
+            for(i=0;i<CANT_EMPLEADOS;i++)
+            {
+                auxEmployee=newEmployee(i,"Carlitos","Andromeda",6995+i,i);
+                ll_add(test,auxEmployee);
+            }
+            for(i=0;i<CANT_EMPLEADOS;i++)
+            {
+                auxEmployee=ll_get(test,i);
+                printf("Nombre: %s,Sueldo: %.2f\n",auxEmployee->name,auxEmployee->salary);
+            }
+            newTest=ll_filter(test,compareEmployee2,1);
+            if(newTest!=NULL)
+            {
+                size=ll_len(newTest);
+                printf("\nLa lista filtrada es:\n");
+                for(i=0;i<size;i++)
+                {
+                    auxEmployee=ll_get(newTest,i);
+                    if(auxEmployee!=NULL)
+                    {
+                        printf("Nombre: %s,Sueldo: %.2f\n",auxEmployee->name,auxEmployee->salary);
+                    }else
+                    {
+                        printf("Es NULL\n");
+                    }
+                }
+            }
+        }
+
+}
---
+++ TP4 agregados/linkedlist_cascara_v5/src/owntest.h
@@ -0,0 +1,6 @@
+#ifndef OWNTEST_H_INCLUDED
+#define OWNTEST_H_INCLUDED
+
+void testFilter(void);
+
+#endif // OWNTEST_H_INCLUDED
